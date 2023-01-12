0797fe63aa391bebee0e205982c108b4d058c225
Aditya KUmar <113716943+adityakumar002216@users.noreply.github.com>
2023-01-12 15:27:51 +0530

    Create Day_4_Minimize_the_sum.cpp

---
+++ Problem_of_the_day/Day_4_Minimize_the_sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    long minimizeSum(int N, int arr[]) {
+        // code here
+        long k=0;
+        PriorityQueue<Integer>s=new PriorityQueue<>();
+        for(int i:arr)
+        s.add(i);
+        while(s.size()>1){
+           int curr=s.remove()+s.remove();
+           k+=curr;
+           s.add(curr);
+        }
+        return k;
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0; i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            long ans = obj.minimizeSum(n, A);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
