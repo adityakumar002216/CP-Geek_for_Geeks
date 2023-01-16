a07ecb1f74cb239ec83a1d5e2bcf2990aef5a8b1
Aditya Kumar <113716943+adityakumar002216@users.noreply.github.com>
2023-01-16 10:25:36 +0530

    Create Day_8_next_greater_element.cpp

---
+++ Problem_of_the_day/Day_8_next_greater_element.cpp
@@ -0,0 +1,25 @@
+ stack<int>st;
+        st.push(n-1);
+        vector<long long>ans;
+        ans.push_back(-1);
+        
+        for(int i=n-2;i>=0; i--)
+        {
+            while(!st.empty() and arr[st.top()]<=arr[i])
+            {
+                st.pop();
+            }
+            if(!st.empty())
+            {
+                ans.push_back(arr[st.top()]);
+            }
+            else
+            {
+                ans.push_back(-1);
+            }
+            
+            st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
