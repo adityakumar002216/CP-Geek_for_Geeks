9c1d82b71d5566b5352a31f75671abc56a2f99d4
Aditya Kumar <113716943+adityakumar002216@users.noreply.github.com>
2023-03-05 21:48:43 +0530

    Create immidate_smaller_element.cpp

---
+++ Basic_attempt/immidate_smaller_element.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:	
+	void immediateSmaller(vector<int>&arr, int n) {
+	    //  code here
+	    for(int i=1;i<n;i++){
+	        if(arr[i-1]>arr[i])
+	        arr[i-1]=arr[i];
+	        else
+	        arr[i-1]=-1;
+	        
+	    }
+	    arr[n-1]=-1;
+	}
+
+};
