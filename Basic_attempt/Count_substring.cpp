6b6ce477b0d90f99e8c82f8926861ae938b05c76
Aditya Kumar <113716943+adityakumar002216@users.noreply.github.com>
2023-03-29 21:57:41 +0530

    Create Count_substring.cpp

---
+++ Basic_attempt/Count_substring.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    int countSubstring(string S)
+    {
+        // code here
+        int l,u,t=0;
+        for(int i=0;i<S.length();i++){
+            l=0,u=0;
+        for(int j=i;j<S.length();j++){
+            if(islower(S[j]))
+            l++;
+            else
+            u++;
+            if(l==u)
+            t++;
+        }
+        }
+        
+        return t;
+    }
+};
