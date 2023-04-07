995dfaacdd88e26f9f639e26d1c09b9972a04425
Aditya Kumar <113716943+adityakumar002216@users.noreply.github.com>
2023-04-07 22:07:38 +0530

    Create 60.cpp

---
+++ Problem_of_the_day/60.cpp
@@ -0,0 +1,27 @@
+//User function Template for C++
+
+class Solution{   
+public:
+    int addMinChar(string str){    
+        //code here
+          int len = str.length();
+        int start = 0, end = len - 1;
+        int res = 0;
+        
+        while(start < end) {
+            
+            if(str[start] == str[end]) start++, 
+                                       end--;
+            
+            else {
+                res++;
+                start = 0;
+                end = len - res - 1;
+            }
+            
+        }
+        
+        return res;
+        
+    }
+};
